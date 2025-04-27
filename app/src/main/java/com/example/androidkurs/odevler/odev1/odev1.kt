f4756298c78c6535895888d25690103cce40c650
Fatmabegum Demir <fatmabegumdemir828@gmail.com>
2025-04-27 03:47:12 +0300

    Odev1 yuklendi

---
+++ app/src/main/java/com/example/androidkurs/odevler/odev1/odev1.kt
@@ -0,0 +1,101 @@
+package com.example.androidkurs.odevler.odev1
+
+fun main() {
+    val sehir = "Bursa"
+    val ulke = "Turkiye"
+    val telefon = "0511 111 11 11"
+    val postaKodu = "18000"
+    val email = "ornekbiremail@gmail.com"
+    val meslek = "Bilgisayar Mühendisi"
+    val stokMiktari = 17800
+    val musteriAdi = "Fatmabegum"
+    val bakiye = 50000.5
+    val dogumGunu = "2000-01-01"
+    val maas = 100000
+    val medeniDurum = "Bekar"
+    val urunYorum = "Fiyatını hakeden bir ürün, pişman olmazsınız."
+    val odemeTarihi = "2025-04-22"
+    val odeme = 780.65
+    val siparisAdeti = 12
+    val arabaModeli = "Range Rover Sport"
+    val kitapAdi = "Cesur Yeni Dunya"
+    val yayinlanmaTarihi = "2025-04-27"
+    val indirimMiktari = 52.5
+    val odaSayisi = 5
+    val enlem = 51.7592
+    val boylam = 19.4550
+    val urunAdi = "Kitap"
+    val yemekFiyati = 375.50
+    val marka = "JBL"
+    val muzikAdi = "Voile"
+    val videoSuresi = "15 dakika 25 saniye"
+    val urunPuani = 5
+    val resimAdi = "Cicek Bahcesi"
+    val dosyaFormati = "PDF"
+    val renk = "Turkuaz"
+    val renkKodu = "#40E0D0"
+    val telefonModeli = "Galaxy A35"
+    val ekranBoyutu = 15.6
+    val agirlik = 5.5
+    val ulusalGun = "19 Mayis Genclik ve Spor Bayramı"
+    val tatilGunu = "1 Mayis İsci Bayrami "
+    val rezervasyonTarihi  = "2025-05-15"
+    val sokakAdi = "Cicek"
+    val otobusHatti = "B16"
+    val kalanDakika = 55
+    val takipKodu = "12549"
+    val kuponSuresi = "50 gun"
+    val kuponKodu = "INDIRIM55"
+    val faturaAdresi = "Bilecik Seyh Edebali Universitesi"
+
+    println(sehir)
+    println(ulke)
+    println(telefon)
+    println(postaKodu)
+    println(email)
+    println(meslek)
+    println(stokMiktari)
+    println(musteriAdi)
+    println(bakiye)
+    println(dogumGunu)
+    println(maas)
+    println(medeniDurum)
+    println(urunYorum)
+    println(odemeTarihi)
+    println(odeme)
+    println(siparisAdeti)
+    println(arabaModeli)
+    println(kitapAdi)
+    println(yayinlanmaTarihi)
+    println(indirimMiktari)
+    println(odaSayisi)
+    println(enlem)
+    println(boylam)
+    println(urunAdi)
+    println(yemekFiyati)
+    println(marka)
+    println(muzikAdi)
+    println(videoSuresi)
+    println(urunPuani)
+    println(resimAdi)
+    println(dosyaFormati)
+    println(renk)
+    println(renkKodu)
+    println(telefonModeli)
+    println(ekranBoyutu)
+    println(agirlik)
+    println(ulusalGun)
+    println(tatilGunu)
+    println(rezervasyonTarihi)
+    println(sokakAdi)
+    println(otobusHatti)
+    println(kalanDakika)
+    println(takipKodu)
+    println(kuponSuresi)
+    println(kuponKodu)
+    println(faturaAdresi)
+
+
+
+
+}
